1fb114534809f99f442f16821ed22f776d0854f4
AguilarDavid <aguilardavid19@hotmail.com>
2023-04-19 22:10:21 -0400

    inicializando

---
+++ m3_s3/Paquete/Evaluacion.py
@@ -0,0 +1,10 @@
+#importar modulo math para poder llamar a la funcion math.sqrt()
+import math
+
+#declaracion de variable
+y = 81 #asignando valor a la variable
+
+raiz = math.sqrt(y) #acumulando la raíz cuadrada de la variable  y en una variable llamada raíz
+
+# impresión  en cónsola de la variable raíz
+print(f"La raíz cuadrada de {y} es : {raiz}")
